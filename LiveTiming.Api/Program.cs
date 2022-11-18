5451297e8ebd19cc3fa19780f2383f559c594aee
erik van appeldoorn <erik@nforza.nl>
2022-11-18 22:29:04 +0100

    Included mediatr in API project

--- LiveTiming.Api/Program.cs
+++ LiveTiming.Api/Program.cs
@@ -1,6 +1,15 @@
+using LiveTiming.Application.Queries;
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMediatR(typeof(Program));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (string driver, int lap, IMediator mediator) =>
+{
+    var result = await mediator.Send(new TimedLapQuery {Driver = driver, Lap = lap});
+    return result;
+});
 
 app.Run();
--- LiveTiming.Application/Queries/TimedLapQuery.cs
+++ LiveTiming.Application/Queries/TimedLapQuery.cs
@@ -2,5 +2,6 @@ namespace LiveTiming.Application.Queries;
 
 public class TimedLapQuery
 {
-    
+    public required string Driver { get; init; }
+    public int Lap { get; init; }
 }
