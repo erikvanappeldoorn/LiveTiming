65170fcebc477a4da620996a3f41abc35fe64498
erik van appeldoorn <erik@nforza.nl>
2022-11-28 12:57:41 +0100

    added open.api and swagger

--- LiveTiming.Api/Program.cs
+++ LiveTiming.Api/Program.cs
@@ -6,12 +6,21 @@ using MediatR;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", async (string driver, IMediator mediator) =>
 {
     var result = await mediator.Send(new TimedLapQuery {Driver = driver});
     return result;
-});
+}).WithOpenApi();
 
 app.Run();
